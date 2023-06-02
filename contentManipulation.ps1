b7ae89ba2ead2ba80ad3d4bf6d9f79c84114bfe2
Ryan O'Boyle <37898539+Ryan7G@users.noreply.github.com>
2023-06-02 10:54:30 +0100

    Add files via upload

---
+++ contentManipulation.ps1
@@ -0,0 +1,9 @@
+﻿#Simple script that will modify a files contents with a specified string 
+ $filePath = 'some\path\here'
+    $tempFilePath = "$env:TEMP\$($filePath | Split-Path -Leaf)"
+    $find = 'string to replace'
+    $replace = 'replace with this'
+
+    (Get-Content -Path $filePath) -replace $find, $replace | Add-Content -Path $tempFilePath
+    Remove-Item -Path $filePath
+    Move-Item -Path $tempFilePath -Destination $filePath
